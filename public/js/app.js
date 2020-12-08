f9214234a947a15fba7286f6b5ad8e353a175368
Edward Stoner <edwardlstoner@gmail.com>
2020-12-08 08:20:29 -0700

    Modify weather message

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("here is my js file");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,7 @@ const request = require("request");
 const forecast = ({ latitude: lat, longitude: long }, cb) => {
   const url = `http://api.weatherstack.com/current?access_key=785072119bee7a1b3df4c1df922e0d80&%20query=${lat},${long}&units=f`;
   request({ url, json: true }, (error, { body }) => {
+    console.log(body.current);
     if (error) {
       cb("Unable to connect to weather service!", undefined);
     } else if (body.error) {
@@ -10,7 +11,7 @@ const forecast = ({ latitude: lat, longitude: long }, cb) => {
     } else {
       cb(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The wind is currently blowing at ${body.current.wind_speed} mph.`
       );
     }
   });
